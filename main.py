42797d0cf0480b59d4731ed781a409af0dbea925
Sanghyun Choi <sanghyunc7@gmail.com>
2024-03-20 01:14:37 -0400

    sample_rate + exclude_dir

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import subprocess
 # root directory for music
 IN_DIR = sys.argv[1]
 OUT_DIR = "/mnt/f/HiRes"
+EXCLUDE_DIRS = ["__MACOSX"]
 INPUT_FORMATS = "8svx aif aifc aiff aiffc al amb au avr cdda cdr cvs cvsd cvu dat dvms f32 f4 f64 f8 flac fssd gsrt hcom htk ima ircam la lpc lpc10 lu maud mp2 mp3 nist prc raw s1 s16 s2 s24 s3 s32 s4 s8 sb sf sl sln smp snd sndr sndt sou sox sph sw txw u1 u16 u2 u24 u3 u32 u4 u8 ub ul uw vms voc vox wav wavpcm wve xa".split()
 
 if not IN_DIR.startswith("/"):
@@ -18,6 +19,8 @@ def get_all_files(parent_dir):
     all_files = []
     # Walk through all directories and files recursively
     for root, dirs, files in os.walk(parent_dir):
+        # avoid going into these subdirectories
+        dirs[:] = [d for d in dirs if d not in EXCLUDE_DIRS]
         all_dir.append(root)
         for file in files:
             file_path = os.path.join(root, file)
@@ -66,7 +69,9 @@ def upsample(file):
         return
     
 
-    cmd = f"./sox -S -V6 {file} -b 24 -r 176400 {out_file} upsample 4 sinc -22050 -n 8000000 -L -b 0 vol 4".split()
+    cmd = "./sox -S -V6 placeholder1 -b 24 -r 176400 placeholder2 upsample 4 sinc -22050 -n 8000000 -L -b 0 vol 4".split()
+    cmd[cmd.index("placeholder1")] = file # do this instead of f-strings because we don't want to split the file with spaces
+    cmd[cmd.index("placeholder2")] = out_file
     result = subprocess.run(cmd, capture_output=True)
     print(result.stdout.decode())
 
--- playground/test.py
+++ playground/test.py
@@ -2,14 +2,28 @@
 import os
 import sys
 import subprocess
-for r, d, files in os.walk("/home/dan/Music/classic/Bach/"):
+
+
+sample_rates = []
+for r, dirs, files in os.walk("/home/dan/Music/classic/john/Tchaikovsky"):
+    print(r)
+    
+    dirs[:] = [d for d in dirs if d not in "__MACOSX"]
+
     for f in files:
-        f = "Pablo Casals-06-Bach _ Partita No.1 In B Flat Major BWV.825 - IV. Sarabande.mp3"
-        print(f)
+        if not (f.endswith(".mp3") or f.endswith(".flac")):
+            continue
+        # f = "Pablo Casals-06-Bach _ Partita No.1 In B Flat Major BWV.825 - IV. Sarabande.mp3"
+        file = os.path.join(r, f)
+        print(file)
         cmd = f"sox --info f1 \n".split()
-        cmd[2] = f
-        # cmd = "pwd".split()
+        cmd[2] = file
+
         result = subprocess.run(cmd, capture_output=True)
-        print(result.stderr.decode())
         print(result.stdout.decode())
-sample_rate = ""
+        res = [item.decode("utf-8") for item in result.stdout.split()]
+        print(res)
+        sample_rates.append(res[res.index("Rate") + 2])
+        
+
+print(sample_rates)
