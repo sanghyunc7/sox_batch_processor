3cacb832fef7d83908ef5f95debbdbb1aab3841a
dan choi <dan@windows_pc>
2024-05-14 18:52:26 -0400

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Get the parent directory of the script's location
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
+# Step 1: Extract sox_installation.tar.gz
+tar -xzvf "$SCRIPT_DIR/sox_installation.tar.gz" -C "$SCRIPT_DIR"
+
+# Step 3: Compiling sox
+# Add necessary environment variables to ~/.bashrc and source it
+echo "export PKG_CONFIG=\"$SCRIPT_DIR/local/lib/pkgconfig\"" >> ~/.bashrc
+echo "export PKG_CONFIG_PATH=\"$SCRIPT_DIR/local/lib/pkgconfig\"" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=\"$SCRIPT_DIR/local/lib\"" >> ~/.bashrc
+echo "export PATH=\"$PATH:$SCRIPT_DIR/local/bin\"" >> ~/.bashrc
+echo "export FLAC_CFLAGS=\"-I$SCRIPT_DIR/flac-1.4.3/include\"" >> ~/.bashrc
+echo "export FLAC_LIBS=\"-L$SCRIPT_DIR/local/lib -lFLAC\"" >> ~/.bashrc
+source ~/.bashrc
+
+# Clone sox repository
+if ! git clone https://github.com/rhgg2/sox.git "$SCRIPT_DIR/../sox"; then
+    echo "Error: Failed to clone SoX repository"
+    exit 1
+fi
+cd "$SCRIPT_DIR/../sox" || exit 1
+
+# Install required packages from apt
+sudo apt-get install -y libtool autoconf pkg-config g++ autoconf-archive make
+
+# Run autoreconf to generate configure script
+autoreconf -ivf
+
+# Configure and install sox
+./configure LDFLAGS=-L"$SCRIPT_DIR/local/lib" CFLAGS=-I"$SCRIPT_DIR/local/include" --prefix="$SCRIPT_DIR/local" --with-mad --with-lame --enable-mp3 --enable-flac
+make -s
+make install
+
+echo "SoX has been compiled and installed successfully!"
